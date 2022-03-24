cecb306bd43b2fd815389fed4f0d4c495858e78c
Hưng Randy <89236484+hungdgdhhgfhgfjh@users.noreply.github.com>
2022-03-24 18:21:22 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,15 +25,6 @@ class UserSeeder extends Seeder
         $user->birthday     = '1998/08/14';
         $user->password     = Hash::make("admin123")        ;
         $user->save();    
-        $user->name         = "Nguyễn Ngọc Khánh Vũ";
-        $user->role         = 'Giám đốc';
-        $user->email        = 'admin@gmail.com';
-        $user->phone        = '0915234678';
-        $user->gender       = 'Nam';
-        $user->address      = 'Đông Hà - Quảng Trị' ;
-        $user->birthday     = '1998/18/03';
-        $user->password     = Hash::make("admin123")        ;
-        $user->save();    
     }
  
 }
